e1f6e2205cb39effbc91bc0778f650763ef2e0f1
Emmanuel Eze <45445963+realemmanueleze@users.noreply.github.com>
2024-05-01 15:50:01 +0100

    login controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,8 +10,25 @@ const register = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
-const login = (req, res, next) => {
-  res.status(200).json({ data: req.body });
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+  const user = await User.findOne({ email });
+  // check if user exists
+  if (!user) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+  // compare password
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+  //create token
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = {
